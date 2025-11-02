572d550b235de4546cdb2a7f4ed7372f2355d497
Ragib Asif <ragibasif@tuta.io>
2025-11-02 16:00:16 -0500

    feat: add geopandas to read geojson data

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+import geopandas
 import pandas as pd
 
 
-def main():
-    df = pd.read_csv("data/Bi_Annual_Pedestrian_Counts_20251009.csv")
+def ped_count_csv():
+    df = pd.read_csv("data/count.csv")
     print(df)
+
+
+def ped_count_geojson():
+    df = geopandas.read_file("data/count.geojson")
+    print(df)
+
+
+def ped_demand_csv():
+    df = pd.read_csv("data/demand.csv")
+    print(df)
+
+
+def ped_demand_geojson():
+    df = geopandas.read_file("data/demand.geojson")
+    print(df)
+
+
+def main():
+    ped_count_csv()
+    ped_count_geojson()
+    ped_demand_csv()
+    ped_demand_geojson()
 
 
 if __name__ == "__main__":
