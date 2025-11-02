91638a4d414464e1e84c708c65af8716e9d2512f
Ragib Asif <ragibasif@tuta.io>
2025-11-02 15:41:31 -0500

    feat: init project with uv

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+import pandas as pd
+
+
+def main():
+    df = pd.read_csv("data/Bi_Annual_Pedestrian_Counts_20251009.csv")
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
